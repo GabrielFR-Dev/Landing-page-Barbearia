8934299b77ce136c461715c49cd19a578fd5cb6c
Gabriel Ferreira <164042709+GabrielFR-Dev@users.noreply.github.com>
2025-10-07 17:19:59 -0300

    estruturando o react
    Importando elementos, fontes, icones, paleta de cores.

--- landing-page-barbearia/src/app/page.js
+++ landing-page-barbearia/src/app/page.js
@@ -1,3 +1,8 @@
+'use client'
+import { useState } from "react";
+import Image from "next/image";
+import { BsSun, BsMoon } from "react-icons/bs";
+import Logo from "/public/barbearia.png";
 import estilos from "./page.module.css";
 
 export default function Home() {
