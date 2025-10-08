a3f38bcdcce6999dbf4082c75ccecb3e966a805f
Gabriel Ferreira <164042709+GabrielFR-Dev@users.noreply.github.com>
2025-10-08 18:19:03 -0300

    estilizando a pagina

--- landing-page-barbearia/src/app/page.js
+++ landing-page-barbearia/src/app/page.js
@@ -6,38 +6,38 @@ import Logo from "/public/barbearia.png";
 import estilos from "./page.module.css";
 
 export default function Home() {
-  const [tema, setTema] = useState(false);
+  const [alterarTema, setAlterarTema] = useState(false);
 
-  function alternarTema() {
-    setTema(!tema);
+  function trocarTemaPagina() {
+    setAlterarTema(!alterarTema);
   }
 
 
   return (
-    <div className={alternarTema ? estilos.escuro : estilos.claro}>
+    <div className={alterarTema ? estilos.escuro : estilos.claro}>
       <header className={estilos.container_topo}>
         <div className={estilos.topo}>
-        <Image src={Logo} className={estilos.img}/>
-          <button onClick={alternarTema}>{tema ? <BsSun /> : <BsMoon />}</button>
+          <Image src={Logo} className={estilos.img} />
+          <button onClick={trocarTemaPagina}> {alterarTema ? <BsSun /> : <BsMoon />}</button>
         </div>
       </header>
 
       <section className={estilos.banner}>
       </section>
 
-      <section className={estilos.container_conteudo}>
-        <main>
+      <section className={estilos.secao_conteudo}>
+        <main className={estilos.container_conteudo}>
           <h1>Bem-vindo a Barber Shop</h1>
 
-          <p>Nossa barbearia sempre oferece profissionais de qualidade e estamos prontos para lidar com suas maiores expectativas</p>
+          <p className={estilos.paragrafo}> Nossa barbearia sempre oferece profissionais de qualidade e estamos prontos para lidar com suas maiores expectativas</p>
 
-          <p>Nossos serviços são dedicados ao seu sucesso pessoal. Aqui temos uma equipe premiada que demonstrou o talento de mestres barbeiros em vários concursos de estilo. Deixe nosso barbeiro ser seu estilista pessoal e você nunca ficará desapontado.</p>
+          <p className={estilos.paragrafo}> Nossos serviços são dedicados ao seu sucesso pessoal. Aqui temos uma equipe premiada que demonstrou o talento de mestres barbeiros em vários concursos de estilo. Deixe nosso barbeiro ser seu estilista pessoal e você nunca ficará desapontado.</p>
 
-          <p className={estilos.fonte_assinatura}>
-            S. Gabriel
-          </p>
-
+            <p className={estilos.fonte_assinatura}>
+              S. Gabriel
+            </p>
         </main>
+
       </section>
 
     </div>
