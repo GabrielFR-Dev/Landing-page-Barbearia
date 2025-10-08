bc116daa9f715259fd0d9a00d962ebc83303065d
Gabriel Ferreira <164042709+GabrielFR-Dev@users.noreply.github.com>
2025-10-08 19:05:05 -0300

    projeto finalizado
    Estilizei a página, fiz algumas alterações no código e coloquei responsividade no projeto utilizando media query

--- landing-page-barbearia/src/app/page.js
+++ landing-page-barbearia/src/app/page.js
@@ -29,9 +29,9 @@ export default function Home() {
         <main className={estilos.container_conteudo}>
           <h1>Bem-vindo a Barber Shop</h1>
 
-          <p className={estilos.paragrafo}> Nossa barbearia sempre oferece profissionais de qualidade e estamos prontos para lidar com suas maiores expectativas</p>
+          <p className={estilos.paragrafo}> Nossa barbearia sempre oferece profissionais de qualidade e estamos prontos para lidar com suas maiores expectativas.</p>
 
-          <p className={estilos.paragrafo}> Nossos serviços são dedicados ao seu sucesso pessoal. Aqui temos uma equipe premiada que demonstrou o talento de mestres barbeiros em vários concursos de estilo. Deixe nosso barbeiro ser seu estilista pessoal e você nunca ficará desapontado.</p>
+          <p className={estilos.paragrafo2}> Nossos serviços são dedicados ao seu sucesso pessoal. Aqui temos uma equipe premiada que demonstrou o talento de mestres barbeiros em vários concursos de estilo. Deixe nosso barbeiro ser seu estilista pessoal e você nunca ficará desapontado.</p>
 
             <p className={estilos.fonte_assinatura}>
               S. Gabriel
